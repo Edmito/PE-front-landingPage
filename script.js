0762c5ed668494b18eb94832760aca297f02dec6
Edmito <edmilsongandra10@gmail.com>
2023-10-31 00:33:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// Navegação
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+
+hamburger.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
+// Banner
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+let autoSlideInterval;
+
+function showSlide(slideIndex) {
+  slides[currentSlide].classList.remove('active');
+  slides[slideIndex].classList.add('active');
+  currentSlide = slideIndex;
+}
+
+function nextSlide() {
+  const nextSlide = (currentSlide + 1) % slides.length;
+  showSlide(nextSlide);
+}
+
+function prevSlide() {
+  const prevSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(prevSlide);
+}
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(nextSlide, 6000);
+}
+
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
+}
+
+document.querySelector('.prev').addEventListener('click', function () {
+  prevSlide();
+  stopAutoSlide();
+  startAutoSlide();
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  nextSlide();
+  stopAutoSlide();
+  startAutoSlide();
+});
+
+startAutoSlide();
+showSlide(currentSlide);
